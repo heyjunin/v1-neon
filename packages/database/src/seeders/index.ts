ba9c39c1655ba92f1fd53c7abf653631f1a99d4d
Júnior Araújo <gomezdigitalpro@proton.me>
2025-08-19 21:24:07 -0300

    feat: add DatabaseSeeder to seeders index for improved database seeding

--- packages/database/src/seeders/index.ts
+++ packages/database/src/seeders/index.ts
@@ -1,14 +1,17 @@
 export * from './base-seeder';
+export * from './database-seeder';
 export * from './orchestrator';
 export * from './posts-seeder';
 export * from './types';
 export * from './users-seeder';
 
 // Re-export all seeders for easy importing
+import { DatabaseSeeder } from './database-seeder';
 import { PostsSeeder } from './posts-seeder';
 import { UsersSeeder } from './users-seeder';
 
 export const seeders = [
+  new DatabaseSeeder(),
   new UsersSeeder(),
   new PostsSeeder()
 ];
